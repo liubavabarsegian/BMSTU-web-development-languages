c1407e2f6aa314c9ba6c3f39e594d2510ab2b8d4
liubavabarsegian <lyubahemmo@mail.ru>
2022-09-17 22:50:27 +0300

    adding to basket is done

--- labs/lab4/order.js
+++ labs/lab4/order.js
@@ -1,7 +1,10 @@
 tableMenu = document.getElementById("table-menu");
-btnElement = document.querySelector(".btn");
+tableBasket = document.getElementById("table-basket");
+btnElement = document.getElementById("add-btn");
 nameInput = document.querySelector(".name__input");
 priceInput = document.querySelector(".price__input");
+tableBasketBackground = document.querySelector(".shopping-cart");
+countBasketGoods = 0;
 
 btnElement.addEventListener( "click", function(){
 	var goodsName = nameInput.value;
@@ -19,18 +22,62 @@ function addRow(goodsName, goodsPrice) {
 	cell.innerHTML = goodsName;
 	cell = row.insertCell(2);
 	cell.innerHTML = goodsPrice + " руб.";
+	cell.setAttribute("name", goodsName);
+	cell.setAttribute("value", goodsPrice);
 
 	nameInput.value = "";
 	priceInput.value = "";
+
+	rowClick();
 }
-// function rowClick() {
-//     var rows = document.getElementById("table-menu").rows;
-//     for (i = 0; i < rows.length; i++) {
-//         rows[i].onclick = function(){ return function(){
-//             	var value = this.cells[2].innerHTML.value;
-//             	alert("id:" + value);
-//         };}(rows[i]);
-//     }
-// }
-// window.onload = rowClick();
+function rowClick() {
+    var rows = document.getElementById("table-menu").rows;
+    for (i = 1; i < rows.length; i++) {
+        rows[i].onclick = function(){
+			var value = this.cells[2].getAttribute("value");
+			var name = this.cells[2].getAttribute("name");
+			addToBasket(name, value);
+        };
+    }
+}
+
+function addToBasket(name, price) {
+	countBasketGoods += 1;
+
+	var row = tableBasket.insertRow();
+	var cell = row.insertCell(0);
+	cell.innerHTML = countBasketGoods;
+	var cell = row.insertCell(1);
+	cell.innerHTML = name;
+	cell.setAttribute("name", name);
+	cell = row.insertCell(2);
+	cell.setAttribute("value", price);
+	cell.innerHTML = price + " руб.";
+
+	tableBasketBackground.querySelector("p").textContent = "";
+}
+
+window.onload = rowClick();
+
+function clearBusket() {
+	var rows = document.getElementById("table-basket").rows;
+    for (i = rows.length - 1; i >= 0; i--) {
+        rows[i].remove();
+    }
+	tableBasketBackground.querySelector("p").textContent = "Корзина пуста:( Вы пока ничего не добавили!";
+	countBasketGoods = 0;
+}
+
+function countTotalCost() {
+	totalSum = 0;
+	var rows = document.getElementById("table-basket").rows;
+    for (i = 0; i < rows.length; i++) {
+        var price = rows[i].getAttribute("value");
+		totalSum += price;
+    }
+	alert(totalSum);
+	result = tableBasketBackground.createElement("p");
+	result.textContent = "Итого: " + totalSum + " руб."
+}
+
 
