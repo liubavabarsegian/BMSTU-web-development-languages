afe6a963c79c41a25d2b93c881501558011d0935
liubavabarsegian <lyubahemmo@mail.ru>
2022-09-30 22:48:12 +0300

    started

---
+++ labs/lab5/calc.rb
@@ -0,0 +1,5 @@
+module Calculate
+    def Calc(x)
+        y = Math.sin(x)/(x ** 3 - 5) + (x ** 2) - 2 * Math.exp(x)
+    end
+end
---
+++ labs/lab5/main.rb
@@ -0,0 +1,4 @@
+require_relative 'calc.rb'
+include Calculate
+
+puts Calc(0)
