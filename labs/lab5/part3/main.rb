9d7c2a12c6e0b768496b4562b43d62c1923cb45e
liubavabarsegian <lyubahemmo@mail.ru>
2022-10-10 22:28:25 +0300

    improved code

--- labs/lab5/part3/main.rb
+++ labs/lab5/part3/main.rb
@@ -2,18 +2,9 @@
 
 require_relative 'strings'
 
-# module Main
-module Main
-  include Strings
-
-  def self.main
-    Strings.input_strings
-    print "Строки до исправления:\n"
-    Strings.print_strings
-    Strings.correct_strings
-    print "\nСтроки после исправления:\n"
-    Strings.print_strings
-  end
-end
-
-Main.main
+Strings.input_strings
+print "Строки до исправления:\n"
+Strings.print_strings
+Strings.correct_strings
+print "\nСтроки после исправления:\n"
+Strings.print_strings
