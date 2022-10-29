7dbb80301e7690dfa4f8a07b2568328c80cb2ffc
liubavabarsegian <lyubahemmo@mail.ru>
2022-10-29 23:56:08 +0300

    automatic tester

--- labs/lab5/part3/strings_spec.rb
+++ labs/lab5/part3/strings_spec.rb
@@ -8,21 +8,15 @@ RSpec.describe Strings do
   include Faker
   Faker::Config.locale = :ru
   describe '#correct_strings' do
-    context 'when the string is valid' do
-      it 'corrects the string' do
-        strings = ['Наша Таня громко плачет', 'Уронила в речку мячик']
-        expect(Strings.correct_strings(strings)).to eq(['таша Таня громке плачет', 'Уронила в речку мячик'])
-      end
-    end
     context 'when the string is valid / authomatic' do
       it 'corrects the string' do
         first = Faker::Name.first_name
         second = Faker::Name.first_name
         input = Array[first, second]
-        first[0] == 'т' if first[0] == 'Н'|| first[0] == 'н'
-        second[0] == 'т' if second[0] == 'Н'|| second[0] == 'н'
-        first[-1] == 'е' if first[-1] == 'о' || first[-1] == 'О' 
-        second[-1] == 'е' if second[-1] == 'о' || second[-1] == 'О' 
+        first[0] == 'т' if first[0] == 'Н' || first[0] == 'н'
+        second[0] == 'т' if second[0] == 'Н' || second[0] == 'н'
+        first[-1] == 'е' if first[-1] == 'о' || first[-1] == 'О'
+        second[-1] == 'е' if second[-1] == 'о' || second[-1] == 'О'
         expect(Strings.correct_strings(input)).to eq(Array[first, second])
       end
     end
